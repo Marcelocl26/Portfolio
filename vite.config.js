b6ae478b5c4ed30f923f6c5d3f552dbac353da29
Marcelocl26 <mar.atenas@duocuc.cl>
2024-08-15 19:36:43 -0400

    deploys

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://Marcelocl26.github.io/Portfolio'
+   base: '/Portfolio/'
+  
 })
